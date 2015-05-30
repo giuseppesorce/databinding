e6bca5a5ce4452e5adc470ad2da31630d4a8b9a7
giuseppesorce <giuseppe.sorce@warpmobile>
2015-05-30 10:58:00 +0200

    first commit

---
+++ app/src/main/java/com/giuseppesorce/databinding/MainActivity.java
@@ -0,0 +1,41 @@
+package com.giuseppesorce.databinding;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.main_activity);
+        User user = new User("Test", "User");
+        binding.setUser(user);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/giuseppesorce/databinding/models/FieldForm.java
@@ -0,0 +1,12 @@
+package com.giuseppesorce.databinding.models;
+
+/**
+ * Created by devandroid on 30/05/15.
+ */
+public class FieldForm {
+
+    public final String name="Nome";
+    public final String surname="Cognome";
+    public final String address="Indirizzo";
+
+}
